ef912e0b196aecd0d238b2850b79b1c8e0a44b53
Chris Engebretson <cengebretson@globalvetlink.com>
2014-11-14 10:59:02 -0600

    pipe isn't working

--- loadcsv.js
+++ loadcsv.js
@@ -42,9 +42,9 @@ function load(filename, options) {
   return results;
 }
 
-function getColumns(line, del) {
+function getColumns(line, delimiter) {
   console.assert(typeof line === 'string', 'missing header line');
-  var regex   = new RegExp("(\".*?\"\\\s*|[^\"" + del + "]+)(?=\\\s*[" + del + "]|\\\s*$)","g");
+  var regex   = new RegExp("(\".*?\"\\\s*|[^\"" + delimiter + "]+)(?=\\\s*[" + delimiter + "]|\\\s*$)","g");
   var columns = line.match(regex);
   console.assert(columns.length > 1, 'invalid columns ' + JSON.stringify(columns) + ' from line ' + line);
   return stripQuotes(columns);
